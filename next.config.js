204893d7395fa54211339b99c999d176f61d35ad
blackpanther26 <priyanshuchahal1@gmail.com>
2025-12-28 20:02:40 +0530

    add github workflow

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/blackpanther26' : '',
+}
+
+module.exports = nextConfig
